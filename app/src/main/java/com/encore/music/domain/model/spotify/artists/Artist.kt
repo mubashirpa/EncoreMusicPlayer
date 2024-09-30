47a92de7ce8399d9b0381d0502ec8250ddeea128
Mubashir P A <mubashirpa2002@gmail.com>
2024-09-30 23:28:20 +0530

    Add domain models

---
+++ app/src/main/java/com/encore/music/domain/model/spotify/artists/Artist.kt
@@ -0,0 +1,7 @@
+package com.encore.music.domain.model.spotify.artists
+
+data class Artist(
+    val id: String = "",
+    val imageUrl: String = "",
+    val name: String = "",
+)
---
+++ app/src/main/java/com/encore/music/domain/model/spotify/tracks/Track.kt
@@ -0,0 +1,7 @@
+package com.encore.music.domain.model.spotify.tracks
+
+data class Track(
+    val id: String = "",
+    val imageUrl: String = "",
+    val name: String = "",
+)
