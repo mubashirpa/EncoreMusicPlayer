60f92a831c08cc3c0a360a1588a378ed67b28005
Mubashir P A <mubashirpa2002@gmail.com>
2024-10-05 11:52:20 +0530

    Optimised error handling

--- app/src/main/java/com/encore/music/domain/usecase/authentication/CreateUserUseCase.kt
+++ app/src/main/java/com/encore/music/domain/usecase/authentication/CreateUserUseCase.kt
@@ -2,10 +2,11 @@ package com.encore.music.domain.usecase.authentication
 
 import com.encore.music.core.Result
 import com.encore.music.core.UiText
+import com.encore.music.core.utils.AuthenticationUtils
 import com.encore.music.domain.model.authentication.User
 import com.encore.music.domain.repository.AuthenticationRepository
 import com.google.firebase.FirebaseNetworkException
-import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import com.encore.music.R.string as Strings
@@ -24,8 +25,10 @@ class CreateUserUseCase(
                 val user =
                     repository.createUserWithEmailAndPassword(name, email, password)
                 emit(Result.Success(user))
-            } catch (e: FirebaseAuthUserCollisionException) {
-                emit(Result.Error(UiText.StringResource(Strings.auth_error_email_already_in_use)))
+            } catch (e: FirebaseAuthException) {
+                val errorMessage =
+                    AuthenticationUtils.authErrors[e.errorCode] ?: Strings.auth_unknown_exception
+                emit(Result.Error(UiText.StringResource(errorMessage)))
             } catch (e: FirebaseNetworkException) {
                 emit(Result.Error(UiText.StringResource(Strings.auth_network_exception)))
             } catch (e: Exception) {
