7f19f356e8aed183caa316ca0e32a14ff4f37cbe
Mubashir P A <mubashirpa2002@gmail.com>
2024-10-17 11:31:40 +0530

    Add indices

--- app/src/main/java/com/encore/music/data/local/entity/playlists/PlaylistTrackCrossRef.kt
+++ app/src/main/java/com/encore/music/data/local/entity/playlists/PlaylistTrackCrossRef.kt
@@ -1,8 +1,12 @@
 package com.encore.music.data.local.entity.playlists
 
 import androidx.room.Entity
+import androidx.room.Index
 
-@Entity(primaryKeys = ["playlistId", "trackId"])
+@Entity(
+    primaryKeys = ["playlistId", "trackId"],
+    indices = [Index(value = ["trackId"])],
+)
 data class PlaylistTrackCrossRef(
     val playlistId: String,
     val trackId: String,
--- app/src/main/java/com/encore/music/data/local/entity/tracks/TrackArtistCrossRef.kt
+++ app/src/main/java/com/encore/music/data/local/entity/tracks/TrackArtistCrossRef.kt
@@ -1,8 +1,12 @@
 package com.encore.music.data.local.entity.tracks
 
 import androidx.room.Entity
+import androidx.room.Index
 
-@Entity(primaryKeys = ["trackId", "artistId"])
+@Entity(
+    primaryKeys = ["trackId", "artistId"],
+    indices = [Index(value = ["artistId"])],
+)
 data class TrackArtistCrossRef(
     val trackId: String,
     val artistId: String,
