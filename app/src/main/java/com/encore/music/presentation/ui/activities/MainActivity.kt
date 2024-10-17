b9b615e3c669b47fa2faef4101ecadad98de951d
Mubashir P A <mubashirpa2002@gmail.com>
2024-10-17 20:30:21 +0530

    Installed splash screen

--- app/src/main/java/com/encore/music/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/encore/music/presentation/ui/activities/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -41,6 +42,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
