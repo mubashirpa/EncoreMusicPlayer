c133f71b9bfde6504f22ff96a59cc7dccb9b97ea
Mubashir P A <mubashirpa2002@gmail.com>
2024-11-01 20:31:03 +0530

    - Added option to open externally

--- app/src/main/java/com/encore/music/presentation/ui/fragments/library/LibraryFragment.kt
+++ app/src/main/java/com/encore/music/presentation/ui/fragments/library/LibraryFragment.kt
@@ -1,5 +1,7 @@
 package com.encore.music.presentation.ui.fragments.library
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -213,6 +215,11 @@ class LibraryFragment : Fragment() {
                         ),
                     icon = R.drawable.baseline_person_search_24,
                 ),
+                MenuItem(
+                    id = 5,
+                    title = getString(R.string.open_in_spotify),
+                    icon = R.drawable.baseline_open_in_new_24,
+                ),
             )
         TrackMenuBottomSheet(track, items)
             .setOnMenuItemClickListener { _, id ->
@@ -235,6 +242,19 @@ class LibraryFragment : Fragment() {
 
                     4 -> {
                         artist?.id?.let { navController.navigateToArtist(it) }
+                    }
+
+                    5 -> {
+                        track.externalUrl?.let {
+                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(it))
+                            try {
+                                startActivity(intent)
+                            } catch (e: Exception) {
+                                showMessage(getString(R.string.unable_to_open_url))
+                            }
+                        } ?: run {
+                            showMessage(getString(R.string.error_unexpected))
+                        }
                     }
                 }
             }.show(
